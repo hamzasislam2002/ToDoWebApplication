5fa3e749908a1705a9a96db12226a3bf5d3981a2
Hamza Islam <islam.144@osu.edu>
2025-11-16 07:39:09 -0500

    Made changes

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def page():
     return render_template("base.html")
 
-@app.route("/notesform")
-def formnotes():
-    return render_template("notes_form.html")
-
-@app.route("/login")
-def login():
+@app.route("/login", methods=["GET", "POST"])
+def home():
+    
     return render_template("login.html")
 
+@app.route("/notesform")
+def form():
+    return render_template("notes_form.html")
+
 @app.route("/notes")
-def note():
+def notes():
     return render_template("notes.html")
 
 @app.route("/signup")
 def signup():
     return render_template("signup.html")
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("page"))
+
+@app.route("/<name>")
+def fullname(name):
+    return f"<h1>Hello</h1> {name}"
 
 if __name__ == "__main__":
     app.run()
